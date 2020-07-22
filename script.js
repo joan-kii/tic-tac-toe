f5ae87dd2b2cafa76b24d61a0c45bcc98bae2e02
joan-kii <joan.sb81@gmail.com>
2020-07-22 18:37:07 +0200

    module pattern gameController

--- script.js
+++ script.js
@@ -2,17 +2,18 @@
 
 // Create Players
 
-const playerFactory = (name, figure) => {
-    return { name, figure };
+const playerFactory = (name, marker) => {
+    return { name, marker };
 };
 
 // Create Gameboard
 
 const createGameboard = (() => {
-    const gameboard =  [[A1, A2, A3],
-                        [B1, B2, B3],
-                        [C1, C2, C3]];
-    return { gameboard };
+    const gameboard = [];
+    let board = document.getElementById('gameboard').children;
+    for (let cell in board) {
+        board[cell].innerText = gameboard[cell] || "";
+    };
 })();
 
 // Create Display
