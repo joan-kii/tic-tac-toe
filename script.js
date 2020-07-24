c761e2d3595de87b2c21c00ddb18b317ac07d52f
joan-kii <joan.sb81@gmail.com>
2020-07-24 06:18:15 +0200

    añade render y crea jugadores

--- script.js
+++ script.js
@@ -11,18 +11,37 @@ const playerFactory = (name, marker) => {
 const createGameboard = (() => {
     const gameboard = [];
     let board = document.getElementById('gameboard').children;
-    
-    const render = (gameboard) => {
-        for (let cell in board) {
-            board[cell].innerText = gameboard[cell] || "";
-    }};
-
-    
-    render(gameboard);
+    return { gameboard, board }
 })();
 
 // Create Display
 
 const displayController = (() => {
 
-})
+    // Inputs
+
+    const getPlayer1 = document.getElementById("namePlayer1");
+    const getPlayer2 = document.getElementById("namePlayer2");
+
+    // Functions
+
+    let player1 = playerFactory(getPlayer1.value || "Jugador 1", "O");
+    let player2 = playerFactory(getPlayer2.value || "Jugador 2", "X");
+    console.log(player1.name)
+    console.log(player2.marker)
+    const render = () => {
+        for (let tile in createGameboard.board) {
+            createGameboard.board[tile].innerText = createGameboard.gameboard[tile] || "";
+    }};
+
+    const updateGameboard = (cellIndex) => {
+        createGameboard.gameboard[cellIndex] = "O";
+        render(createGameboard.gameboard);
+    };
+
+    for (let cell of createGameboard.board) {
+        cell.addEventListener('click', function () {
+            updateGameboard(cell.getAttribute('data-index'));
+        })
+    };
+})();
