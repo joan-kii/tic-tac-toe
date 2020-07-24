b93ab31e7cf19cd72c3edb981aa7ef4a9097c4b3
joan-kii <joan.sb81@gmail.com>
2020-07-24 18:32:52 +0200

    añade turnos y reinicio

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// Functions
-
 // Create Players
 
 const playerFactory = (name, marker) => {
@@ -20,23 +18,39 @@ const displayController = (() => {
 
     // Inputs
 
-    // Functions
+    for (let cell of createGameboard.board) {
+        cell.addEventListener('click', function () {
+            updateGameboard(cell.getAttribute('data-index'));
+        })
+    };
+    document.getElementById('restartButton').addEventListener("click", restartPlay);
 
+    // Variables
+
+    let move = 1;
     let player1 = playerFactory("Jugador 1", "O");
     let player2 = playerFactory("Jugador 2", "X");
-   
+
+    // Functions
+
+    const render = () => {
         for (let tile in createGameboard.board) {
             createGameboard.board[tile].innerText = createGameboard.gameboard[tile] || "";
     }};
 
     const updateGameboard = (cellIndex) => {
-        createGameboard.gameboard[cellIndex] = "O";
+        createGameboard.gameboard[cellIndex] = toggleTurn(move);
+        move++;
         render(createGameboard.gameboard);
     };
 
-    for (let cell of createGameboard.board) {
-        cell.addEventListener('click', function () {
-            updateGameboard(cell.getAttribute('data-index'));
-        })
+    const toggleTurn = (move) => {
+        return move % 2 != 0 ? player1.marker : player2.marker;
+        };
+
+    function restartPlay() {
+        createGameboard.gameboard = [];
+        move = 1;
+        render()
     };
 })();
